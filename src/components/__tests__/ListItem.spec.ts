d78a7d944607cad92b1a37536462967e16f3681a
Harivansh <hello@harivan.sh>
2023-06-19 13:21:20 +0400

    feat: setup husky

--- src/components/__tests__/ListItem.spec.ts
+++ src/components/__tests__/ListItem.spec.ts
@@ -2,10 +2,12 @@ import { describe, it, expect } from 'vitest';
 
 import { mount } from '@vue/test-utils';
 import ListItem from '../ListItem.vue';
+import { ListItem as Item } from '../../models/list-item';
 
 describe('ListItem', () => {
   it('renders properly', () => {
-    const wrapper = mount(ListItem, { props: { title: 'Item No. 1' } });
-    expect(wrapper.text()).toContain('Item No. 1');
+    const item = new Item(1, "Hello, item!")
+    const wrapper = mount(ListItem, { props: { item } });
+    expect(wrapper.text()).toContain(item.title);
   });
 });

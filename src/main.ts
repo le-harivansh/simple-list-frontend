c7b8900d79e9b41c5d8e3c6e583a5d69ea23fc71
Harivansh <hello@harivan.sh>
2023-06-19 12:21:20 +0400

    feat: setup axios

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import axios from 'axios';
+import { createApp } from 'vue';
+
+import App from './App.vue';
+
 import './assets/main.css';
 
-import { createApp } from 'vue';
-import App from './App.vue';
+axios.defaults.baseURL = "http://localhost:3000";
 
 createApp(App).mount('#app');
